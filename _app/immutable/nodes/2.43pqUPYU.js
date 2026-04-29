864a27ecf4f97f7aba088b1319fe2aef63acb967
Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
2026-04-29 08:21:49 +0200

    use astro

---
+++ _app/immutable/nodes/2.43pqUPYU.js
@@ -0,0 +1,1 @@
+import{U as e,W as t}from"../chunks/Cq3OVONK.js";import"../chunks/DXLwiZ0H.js";import{t as n}from"../chunks/DABDbITq.js";function r(r,i){t(i,!0),n(r,{get latest(){return i.data.latest},get sections(){return i.data.sections}}),e()}export{r as component};
